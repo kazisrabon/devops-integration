2b4b682f05cb73d5fda175c09f3d4e88d9d6cb6b
Kazi Solaiman Ahmed <kaziiit@gmail.com>
2023-05-03 02:12:21 -0500

    controller added to the base url

--- src/main/java/com/kazi/devopsintegration/DevopsIntegrationApplication.java
+++ src/main/java/com/kazi/devopsintegration/DevopsIntegrationApplication.java
@@ -2,9 +2,17 @@ package com.kazi.devopsintegration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DevopsIntegrationApplication {
+
+	@GetMapping
+	public String message(){
+		return "Welcome to KS!!!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsIntegrationApplication.class, args);
